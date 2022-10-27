1a0f5044a31e9eb834936742e0c77e9cb0b2e886
lko23 <107390465+lko23@users.noreply.github.com>
2022-10-27 12:03:23 +0200

    Update check_mssql_database.py

--- check_mssql_database.py
+++ check_mssql_database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ########################################################################
-# check_mssql_database - A Nagios plugin to check Microsoft SQL Server
+# check_mssql_server - A Nagios plugin to check Microsoft SQL Server
 # Copyright (C) 2017 Nagios Enterprises
 #
 # This program is free software; you can redistribute it and/or
@@ -18,9 +18,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-################### check_mssql_database.py ############################
-# Version    : 2.1.2
-# Date       : 06/18/2019
+################### check_mssql_database.py ##############################
+# Version    : 1.0.0
+# Date       : 21/06/2022
 # Maintainer : Nagios Enterprises, LLC
 # License    : GPLv2 (LICENSE.md / https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
 ########################################################################
@@ -35,41 +35,41 @@ except:
     import pickle
 from optparse import OptionParser, OptionGroup
 
-BASE_QUERY = "SELECT cntr_value FROM sys.sysperfinfo WHERE counter_name='%s' AND instance_name='%%s';"
-DIVI_QUERY = "SELECT cntr_value FROM sys.sysperfinfo WHERE counter_name LIKE '%s%%%%' AND instance_name='%%s';"
+BASE_QUERY = "SELECT cntr_value FROM master.dbo.database_monitoring WHERE counter_name='%s' AND instance_name='%%s';"
+DIVI_QUERY = "SELECT cntr_value FROM master.dbo.database_monitoring WHERE counter_name LIKE '%s%%%%' AND instance_name='%%s';"
 
 MODES = {
     
-    'logcachehit'       : { 'help'      : 'Log Cache Hit Ratio',
-                            'stdout'    : 'Log Cache Hit Ratio is %s%%',
-                            'label'     : 'log_cache_hit_ratio',
-                            'unit'      : '%',
-                            'query'     : DIVI_QUERY % 'Log Cache Hit Ratio',
-                            'type'      : 'divide',
-                            'modifier'  : 100,
-                            },
-    
-    'activetrans'       : { 'help'      : 'Active Transactions',
-                            'stdout'    : 'Active Transactions is %s',
-                            'label'     : 'log_file_usage',
-                            'unit'      : '',
-                            'query'     : BASE_QUERY % 'Active Transactions',
-                            'type'      : 'standard',
-                            },
-    
-    'logflushes'         : { 'help'     : 'Log Flushes Per Second',
-                            'stdout'    : 'Log Flushes Per Second is %s/sec',
-                            'label'     : 'log_flushes_per_sec',
-                            'query'     : BASE_QUERY % 'Log Flushes/sec',
-                            'type'      : 'delta'
-                            },
-    
+#    'logcachehit'       : { 'help'      : 'Log Cache Hit Ratio',
+#                            'stdout'    : 'Log Cache Hit Ratio is %s%%',
+#                            'label'     : 'log_cache_hit_ratio',
+#                            'unit'      : '%',
+#                            'query'     : DIVI_QUERY % 'Log Cache Hit Ratio',
+#                            'type'      : 'divide',
+#                            'modifier'  : 100
+#                            },
+#    
+#    'activetrans'       : { 'help'      : 'Active Transactions',
+#                            'stdout'    : 'Active Transactions is %s',
+#                            'label'     : 'log_file_usage',
+#                            'unit'      : '',
+#                            'query'     : BASE_QUERY % 'Active Transactions',
+#                            'type'      : 'standard'
+#                            },
+#    
+#    'logflushes'         : { 'help'     : 'Log Flushes Per Second',
+#                            'stdout'    : 'Log Flushes Per Second is %s/sec',
+#                            'label'     : 'log_flushes_per_sec',
+#                            'query'     : BASE_QUERY % 'Log Flushes/sec',
+#                            'type'      : 'delta'
+#                            },
+   
     'logfileusage'      : { 'help'      : 'Log File Usage',
                             'stdout'    : 'Log File Usage is %s%%',
                             'label'     : 'log_file_usage',
                             'unit'      : '%',
                             'query'     : BASE_QUERY % 'Percent Log Used',
-                            'type'      : 'standard',
+                            'type'      : 'standard'
                             },
     
     'transpsec'         : { 'help'      : 'Transactions Per Second',
@@ -79,73 +79,84 @@ MODES = {
                             'type'      : 'delta'
                             },
     
-    'loggrowths'        : { 'help'      : 'Log Growths',
-                            'stdout'    : 'Log Growths is %s',
-                            'label'     : 'log_growths',
-                            'query'     : BASE_QUERY % 'Log Growths',
-                            'type'      : 'standard'
-                            },
-    
-    'logshrinks'        : { 'help'      : 'Log Shrinks',
-                            'stdout'    : 'Log Shrinks is %s',
-                            'label'     : 'log_shrinks',
-                            'query'     : BASE_QUERY % 'Log Shrinks',
-                            'type'      : 'standard'
-                            },
-    
-    'logtruncs'         : { 'help'      : 'Log Truncations',
-                            'stdout'    : 'Log Truncations is %s',
-                            'label'     : 'log_truncations',
-                            'query'     : BASE_QUERY % 'Log Truncations',
-                            'type'      : 'standard'
-                            },
-    
-    'logwait'           : { 'help'      : 'Log Flush Wait Time',
-                            'stdout'    : 'Log Flush Wait Time is %sms',
-                            'label'     : 'log_wait_time',
-                            'unit'      : 'ms',
-                            'query'     : BASE_QUERY % 'Log Flush Wait Time',
-                            'type'      : 'standard'
-                            },
+#    'loggrowths'        : { 'help'      : 'Log Growths',
+#                            'stdout'    : 'Log Growths is %s',
+#                            'label'     : 'log_growths',
+#                            'query'     : BASE_QUERY % 'Log Growths',
+#                            'type'      : 'standard'
+#                            },
+#    
+#    'logshrinks'        : { 'help'      : 'Log Shrinks',
+#                            'stdout'    : 'Log Shrinks is %s',
+#                            'label'     : 'log_shrinks',
+#                            'query'     : BASE_QUERY % 'Log Shrinks',
+#                            'type'      : 'standard'
+#                            },
+#    
+#    'logtruncs'         : { 'help'      : 'Log Truncations',
+#                            'stdout'    : 'Log Truncations is %s',
+#                            'label'     : 'log_truncations',
+#                            'query'     : BASE_QUERY % 'Log Truncations',
+#                            'type'      : 'standard'
+#                            },
+#    
+#    'logwait'           : { 'help'      : 'Log Flush Wait Time',
+#                            'stdout'    : 'Log Flush Wait Time is %sms',
+#                            'label'     : 'log_wait_time',
+#                            'unit'      : 'ms',
+#                            'query'     : BASE_QUERY % 'Log Flush Wait Time',
+#                            'type'      : 'standard'
+#                            },
     
     'datasize'          : { 'help'      : 'Database Size',
                             'stdout'    : 'Database size is %sKB',
-                            'label'     : 'KB',
+                            'label'     : 'database_size',
                             'query'     : BASE_QUERY % 'Data File(s) Size (KB)',
                             'type'      : 'standard'
                             },
-    
+
+    'datamaxsize'       : { 'help'      : 'Database Maximum Size',
+                            'stdout'    : 'Database maximum size is %sKB',
+                            'label'     : 'database_max_size',
+                            'query'     : BASE_QUERY % 'Data File(s) Max Size (KB)',
+                            'type'      : 'standard'
+                            },
+ 
+    'logsize'           : { 'help'      : 'Log Size',
+                            'stdout'    : 'Log size is %sKB',
+                            'label'     : 'log_size',
+                            'query'     : BASE_QUERY % 'Log File(s) Size (KB)',
+                            'type'      : 'standard'
+                            },
+
+   'datafileusage'      : { 'help'      : 'Data File Usage',
+                            'stdout'    : 'Data File Usage is %s%%',
+                            'label'     : 'data_file_usage',
+                            'unit'      : '%',
+                            'query'     : BASE_QUERY % 'Percent File Used',
+                            'type'      : 'standard'
+                            },
+
     'time2connect'      : { 'help'      : 'Time to connect to the database.' },
     
     'test'              : { 'help'      : 'Run tests of all queries against the database.' },
 }
 
 def return_nagios(options, stdout='', result='', unit='', label=''):
-    invert = False
-    w = options.warning
-    c = options.critical
-
-    # Check for ranges
-    if ':' in options.critical and ':' in options.warning:
-        c = options.critical.split(':')[1]
-        w = options.warning.split(':')[1]
-
-    # Check if we should invert the warning/critical (this should change someday)
-    if int(c) < int(w):
-        invert = True
-
-    if is_within_range(options.critical, result, invert):
+    if is_within_range(options.critical, result):
         prefix = 'CRITICAL: '
         code = 2
-    elif is_within_range(options.warning, result, invert):
+    elif is_within_range(options.warning, result):
         prefix = 'WARNING: '
         code = 1
     else:
         prefix = 'OK: '
         code = 0
-
     strresult = str(result)
-    stdout = stdout % (strresult)
+    try:
+        stdout = stdout % (strresult)
+    except TypeError as e:
+        pass
     stdout = '%s%s|%s=%s%s;%s;%s;;' % (prefix, stdout, label, strresult, unit, options.warning or '', options.critical or '')
     raise NagiosReturn(stdout, code)
 
@@ -158,7 +169,7 @@ class NagiosReturn(Exception):
 class MSSQLQuery(object):
     
     def __init__(self, query, options, label='', unit='', stdout='', host='', modifier=1, *args, **kwargs):
-        self.query = query % options.table
+        self.query = query % options.database
         self.label = label
         self.unit = unit
         self.stdout = stdout
@@ -188,11 +199,14 @@ class MSSQLQuery(object):
 
 class MSSQLDivideQuery(MSSQLQuery):
     
+    def __init__(self, *args, **kwargs):
+        super(MSSQLDivideQuery, self).__init__(*args, **kwargs)
+    
     def calculate_result(self):
-        if self.query_result[1] == 0:
-            self.result = 0
-        else:
+        if self.query_result[1] != 0:
             self.result = (float(self.query_result[0]) / self.query_result[1]) * self.modifier
+        else:
+            self.result = float(self.query_result[0]) * self.modifier
     
     def run_on_connection(self, connection):
         cur = connection.cursor()
@@ -203,7 +217,7 @@ class MSSQLDeltaQuery(MSSQLQuery):
     
     def make_pickle_name(self):
         tmpdir = tempfile.gettempdir()
-        tmpname = hash(self.host + self.options.table + self.query)
+        tmpname = hash(self.host + self.options.database + self.query)
         self.picklename = '%s/mssql-%s.tmp' % (tmpdir, tmpname)
     
     def calculate_result(self):
@@ -230,7 +244,7 @@ class MSSQLDeltaQuery(MSSQLQuery):
             new_val  = self.query_result
             self.result = ((new_val - old_val) / (new_time - old_time)) * self.modifier
         else:
-            self.result = 0
+            self.result = None
         
         new_run = { 'time' : time.time(), 'query_result' : self.query_result }
         
@@ -239,36 +253,15 @@ class MSSQLDeltaQuery(MSSQLQuery):
         pickle.dump(new_run, tmpfile)
         tmpfile.close()
 
-def is_within_range(nagstring, value, invert = False):
-    if not nagstring:
-        return False
-    import re
-    import operator
-    first_float = r'(?P<first>(-?[0-9]+(\.[0-9]+)?))'
-    second_float= r'(?P<second>(-?[0-9]+(\.[0-9]+)?))'
-    actions = [ (r'^%s$' % first_float,lambda y: (value > float(y.group('first'))) or (value < 0)),
-                (r'^%s:$' % first_float,lambda y: value < float(y.group('first'))),
-                (r'^~:%s$' % first_float,lambda y: value > float(y.group('first'))),
-                (r'^%s:%s$' % (first_float,second_float), lambda y: (value < float(y.group('first'))) or (value > float(y.group('second')))),
-                (r'^@%s:%s$' % (first_float,second_float), lambda y: not((value < float(y.group('first'))) or (value > float(y.group('second')))))]
-    for regstr,func in actions:
-        res = re.match(regstr,nagstring)
-        if res: 
-            if invert:
-                return not func(res)
-            else:
-                return func(res)
-    raise Exception('Improper warning/critical format.')
-
 def parse_args():
-    usage = "usage: %prog -H hostname -U user -P password -T table --mode"
+    usage = "usage: %prog -H hostname -U user -P password -D database --mode"
     parser = OptionParser(usage=usage)
     
     required = OptionGroup(parser, "Required Options")
-    required.add_option('-H', '--hostname', help='Specify MSSQL Server Address', default=None)
-    required.add_option('-U', '--user', help='Specify MSSQL User Name', default=None)
-    required.add_option('-P', '--password', help='Specify MSSQL Password', default=None)
-    required.add_option('-T', '--table', help='Specify the table to check', default=None) 
+    required.add_option('-H' , '--hostname', help='Specify MSSQL Server Address', default=None)
+    required.add_option('-U' , '--user', help='Specify MSSQL User Name', default=None)
+    required.add_option('-P' , '--password', help='Specify MSSQL Password', default=None)
+    required.add_option('-D', '--database', help='Specify the database instance to check (default=master)', default='master') 
     parser.add_option_group(required)
     
     connection = OptionGroup(parser, "Optional Connection Information")
@@ -294,7 +287,7 @@ def parse_args():
         parser.error('User is a required option.')
     if not options.password:
         parser.error('Password is a required option.')
-    if not options.table:
+    if not options.database:
         parser.error('Table is a required option.')
     
     if options.instance and options.port:
@@ -309,6 +302,24 @@ def parse_args():
     
     return options
 
+def is_within_range(nagstring, value):
+    if not nagstring:
+        return False
+    import re
+    import operator
+    first_float = r'(?P<first>(-?[0-9]+(\.[0-9]+)?))'
+    second_float= r'(?P<second>(-?[0-9]+(\.[0-9]+)?))'
+    actions = [ (r'^%s$' % first_float,lambda y: (value > float(y.group('first'))) or (value < 0)),
+                (r'^%s:$' % first_float,lambda y: value < float(y.group('first'))),
+                (r'^~:%s$' % first_float,lambda y: value > float(y.group('first'))),
+                (r'^%s:%s$' % (first_float,second_float), lambda y: (value < float(y.group('first'))) or (value > float(y.group('second')))),
+                (r'^@%s:%s$' % (first_float,second_float), lambda y: not((value < float(y.group('first'))) or (value > float(y.group('second')))))]
+    for regstr,func in actions:
+        res = re.match(regstr,nagstring)
+        if res: 
+            return func(res)
+    raise Exception('Improper warning/critical format.')
+
 def connect_db(options):
     host = options.hostname
     if options.instance:
@@ -316,7 +327,7 @@ def connect_db(options):
     elif options.port:
         host += ":" + options.port
     start = time.time()
-    mssql = pymssql.connect(host = host, user = options.user, password = options.password, database=options.table)
+    mssql = pymssql.connect(host = host, user = options.user, password = options.password, database='master')
     total = time.time() - start
     return mssql, total, host
 
@@ -374,6 +385,9 @@ if __name__ == '__main__':
     except pymssql.OperationalError as e:
         print(e)
         sys.exit(3)
+    except pymssql.InterfaceError as e:
+        print(e)
+        sys.exit(3)
     except IOError as e:
         print(e)
         sys.exit(3)
@@ -382,5 +396,5 @@ if __name__ == '__main__':
         sys.exit(e.code)
     except Exception as e:
         print(type(e))
-        print("Caught unexpected error. This could be caused by your sysperfinfo not containing the proper entries for this query, and you may delete this service check.")
+        print("Caught unexpected error. This could be caused by the database_monitoring view not containing the proper entries for this query.")
         sys.exit(3)
